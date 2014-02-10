fc681d704cb6e4205e16f04368a7dca9f0408329
pmlynarczyk <zwarios@gmail.com>
2014-02-10 21:08:32 +0100

    Wyswietlanie pliczorow - do sprawdzenia polozenia bazy fejsikow

--- main.cpp
+++ main.cpp
@@ -12,9 +12,11 @@
 
 
 using namespace std;
+using namespace cv;
 
 int main(int argc, char** argv) {
   
+  // Getting database path
   string orl_database_path;
   
   if(argc > 1) orl_database_path = argv[1];
@@ -23,7 +25,28 @@ int main(int argc, char** argv) {
     return -1;
   }
   
-  cout << endl << orl_database_path << endl;
+  for(int class_counter = 1; class_counter <= 40; class_counter++) {
+    // Getting class database path
+    stringstream sstream;
+    string class_path;
+    sstream << orl_database_path << "/s" << class_counter;
+    sstream >> class_path;
+    
+    for(int image_counter = 1; image_counter <= 10; image_counter++) {
+      // Getting image path
+      stringstream sstream2;
+      string image_path;
+      sstream2 << class_path << "/" << image_counter << ".pgm";
+      sstream2 >> image_path;
+      
+      Mat image;
+      image = imread(image_path, CV_LOAD_IMAGE_COLOR);
+      namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+      imshow( "Display window", image );                   // Show our image inside it.
+      waitKey(0); 
+      
+    }
+  }
   
   
   return 0;
